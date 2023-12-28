db3b5e6334ff1bd8bc64b6209b68e4c74c4dee1c
= <=>
2023-12-28 13:22:03 +0600

    Set up project

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,jsx,ts,tsx}",
+	],
+	
+	theme: {
+		extend: {
+			colors: {
+				"pure-grey-5": "#F9F9F9",
+				"rich-black-5": "#F1F2FF",
+				"rich-black-200": "#999DAA",
+				"rich-black-700": "#2C333F",
+				"rich-black-800": "#161D29",
+				"rich-black-900": "#000814",
+				"yellow-50": "#FFD60A",
+				"yellow-100": "#E7C009",
+				"blue-100": "#47A5C5",
+			},
+			fontFamily: {
+				"edu": ["Edu SA Beginner", "cursive"],
+				"inter": ["Inter", "sans serif"],
+			},
+			boxShadow: {
+				"primary-btn": "-0.5px -1.5px 0px 0px rgba(0, 0, 0, 0.12) inset",
+				"primary-focus": "0px 0px 1px 3px rgba(255, 214, 10, 0.15)",
+			}
+		},
+	},
+	
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")["dark"],
+					"primary": "#FF0000",
+					"secondary": "#47A5C5",
+					"base-100": "#000814",
+					"accent": "#FFD60A",
+				}
+			},
+			"light"
+		]
+	},
+	
+	plugins: [require("daisyui")],
+}
+
