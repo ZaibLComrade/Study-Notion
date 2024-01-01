136011b15396805e9b8a72e9ec293621581dd882
= <=>
2024-01-01 22:39:50 +0600

    Added email-password user authentication

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,29 +1,65 @@
 import auth from "../config/firebase.config";
 import PropTypes from "prop-types";
-import { createContext, useEffect } from "react";
-import {onAuthStateChanged} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	onAuthStateChanged,
+} from "firebase/auth";
 
 export const AuthContext = createContext({});
 
 export default function AuthProvider({ children }) {
+	const [user, setUser] = useState({});
+	const [loading, setLoading] = useState(true);
 	
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, () => {
-			console.log(auth);
-		})
+		const unsubscribe = onAuthStateChanged(auth, (loggedInUser) => {
+			setUser(loggedInUser);
+			
+			// If user is logged in
+			if (loggedInUser) {
+				console.log(loggedInUser);
+			} else { // If user is not logged in
+				console.log(loggedInUser);
+			}
+			
+			setLoading(false);
+		});
 		
 		return () => unsubscribe();
-	}, [])
+	}, []);
 	
-	const authInfo = {
-		test: "This is a test string",
+	const registerUser = (email, password) => {
+		setLoading(false);
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+	
+	const loginUser = (email, password) => {
+		setLoading(false);
+		return signInWithEmailAndPassword(auth, email, password);
 	}
 	
-	return <AuthContext.Provider value={ authInfo }>
-		{ children }
-	</AuthContext.Provider>
+	const logoutUser = () => {
+		setLoading(false);
+		return signOut(auth);
+	}
+	
+	const authInfo = {
+		user,
+		loading,
+		setLoading,
+		registerUser,
+		loginUser,
+		logoutUser,
+	};
+
+	return (
+		<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+	);
 }
 
 AuthProvider.propTypes = {
 	children: PropTypes.node,
-}
+};
